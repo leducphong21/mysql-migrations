b84447da8eb851cf073501c194dd284224eba306
Marek Sokol <marek@raceroster.com>
2021-06-24 15:43:49 +0200

    [RV2-54] Rework mysql-migrations

--- index.js
+++ index.js
@@ -1,78 +1,123 @@
-var fs = require("fs");
+const { setLogger } = require('./lib/logger');
 
-var coreFunctions  = require('./core_functions');
-var queryFunctions  = require('./query');
+let updateSchema = false;
+let migrateAll = false;
 
-var config = require('./config');
-var table = config['table'];
-var migrations_types = config['migrations_types'];
-
-function migration(conn, path, cb) {
-  if(cb == null)
-    cb = () => {};
-
-  queryFunctions.run_query(conn, "CREATE TABLE IF NOT EXISTS `" + table + "` (`timestamp` varchar(254) NOT NULL UNIQUE)", function (res) {
-    handle(process.argv, conn, path, cb);
-  });
-}
-
-function handle(argv, conn, path, cb) {
-  if (argv.length > 2 && argv.length <= 6) {
-    if (argv[2] == 'add' && (argv[3] == 'migration' || argv[3] == 'seed')) {
-      coreFunctions.add_migration(argv, path, function () {
-        conn.end();
-        cb();
-      });
-    } else if (argv[2] == 'up') {
-      var count = null;
-      if (argv.length > 3) {
-        count = parseInt(argv[3]);
-      } else {
-        count = 999999;
-      }
-      coreFunctions.up_migrations(conn, count, path, function () {
-        conn.end();
-        cb();
-      });
-    } else if (argv[2] == 'down') {
-      var count = null;
-      if (argv.length > 3) {
-        count = parseInt(argv[3]);
-      } else count = 1;
-      coreFunctions.down_migrations(conn, count, path, function () {
-        conn.end();
-        cb();
-      });
-    } else if (argv[2] == 'refresh') {
-      coreFunctions.down_migrations(conn, 999999, path, function () {
-        coreFunctions.up_migrations(conn, 999999, path, function () {
-          conn.end();
-          cb();
-        });
-      });
-    } else if (argv[2] == 'run' && migrations_types.indexOf(argv[4]) > -1) {
-      coreFunctions.run_migration_directly(argv[3], argv[4], conn, path, function () {
-        conn.end();
-        cb();
-      });
-    } else {
-      throw new Error('command not found : ' + argv.join(" "));
+const migration = (conn, path, argv = [...process.argv], options = [], customLogger = undefined) => {
+    if (customLogger) {
+      setLogger(customLogger);
     }
-  }
-}
 
-function migrationUp(conn, path, cb) {
-  if (cb == null)
-    cb = () => { };
-  count = 999999;
-  coreFunctions.up_migrations(conn, count, path, function () {
+    const { table } = require('./lib/config');
+    const queryFunctions = require('./lib/query');
+    const logger = require('./lib/logger')();
+
+    const updateSchemaIndex = argv.indexOf("--update-schema");
+    const migrateIndex = argv.indexOf("--migrate-all");
+
+    if (updateSchemaIndex > -1) {
+      updateSchema = true;
+      argv.splice(updateSchemaIndex, 1);
+    }
+
+    if (migrateIndex > -1) {
+      migrateAll = true;
+      argv.splice(migrateIndex, 1);
+    }
+
+    if (options instanceof Array) {
+      if (options.indexOf("--migrate-all") > -1) {
+        migrateAll = true;
+      }
+
+      if (options.indexOf("--update-schema") > -1) {
+        updateSchema = true;
+      }
+    }
+
+    return queryFunctions.runQuery(conn, `CREATE TABLE IF NOT EXISTS \`${table}\` (\`timestamp\` varchar(254) NOT NULL UNIQUE)`)
+      .then(() => handle(argv, conn, path))
+      .catch(error => logger.error(error));
+};
+
+const handle = (argv, conn, path) => new Promise(async (resolve, reject) => {
+  const coreFunctions = require('./lib/core-functions');
+  const { migrationsTypes } = require('./lib/config');
+
+  if (!(argv.length > 2 && argv.length <= 6)) {
+    return resolve();
+  }
+
+  if ('add' === argv[2] && ('migration' === argv[3] || 'seed' === argv[3])) {
+    coreFunctions.addMigration(argv, path);
     conn.end();
-    cb();
-  });
-}
+    return resolve();
+  }
+  else if ('up' === argv[2])
+  {
+    let count = 999999;
+    if (argv.length > 3) {
+      count = parseInt(argv[3]);
+    }
+
+    return coreFunctions[`upMigrations${migrateAll ? 'All' : ''}`](conn, count, path)
+      .then(() => updateSchemaAndEnd(conn, path))
+      .then(() => resolve())
+      .catch(error => reject(error));
+  }
+  else if ('down' === argv[2])
+  {
+    let count = 1;
+    if (argv.length > 3) {
+      count = parseInt(argv[3]);
+    }
+
+    return coreFunctions.downMigrations(conn, count, path)
+      .then(() => updateSchemaAndEnd(conn, path))
+      .then(() => resolve())
+      .catch(error => reject(error));
+  }
+  else if ('refresh' === argv[2])
+  {
+    return coreFunctions.downMigrations(conn, 999999, path)
+      .then(() => coreFunctions.upMigrations(conn, 999999, path))
+      .then(() => updateSchemaAndEnd(conn, path))
+      .then(() => resolve())
+      .catch(error => reject(error));
+  }
+  else if ('run' === argv[2] && migrationsTypes.indexOf(argv[4]) > -1)
+  {
+    coreFunctions.runMigrationDirectly(argv[3], argv[4], conn, path, async () => {
+      await updateSchemaAndEnd(conn, path);
+      return resolve();
+    });
+  }
+  else if ('load-from-schema' === argv[2])
+  {
+    coreFunctions.createFromSchema(conn, path, () => {
+      conn.end();
+      return resolve();
+    });
+  }
+  else {
+    return reject(`command not found: ${argv.join(' ')}`);
+  }
+});
+
+const updateSchemaAndEnd = (conn, path) => new Promise(resolve => {
+  const coreFunctions = require('./lib/core-functions');
+
+  if (updateSchema) {
+    coreFunctions.updateSchema(conn, path, () => {
+      conn.end();
+      return resolve();
+    });
+  } else {
+    conn.end();
+    return resolve();
+  }
+});
 
 module.exports = {
   init: migration,
-  handle,
-  migrationUp,
 }
